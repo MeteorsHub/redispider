7062eb9028a820cf7e31885919adda4e5f4c8a09
MeteorKepler <JimRanor@outlook.com>
2016-08-16 16:28:11 +0800

    build project

---
+++ redispider/redispider/slave.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    redispider.slave
+    ------------------------------------------------------------
+
+    
+
+    :Copyright (c) 2016 MeteorKepler
+    :license: MIT, see LICENSE for more details.
+
+"""
+
+__author__ = 'MeterKepler'
+
+
+class Slave(object):
